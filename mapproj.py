7c4d47c798c2fd6d75afc150053a68ff0cea5c20
Afsaneh <afsaneh639@yahoo.com>
2020-12-02 15:55:21 -0700

    Final version of code file

--- mapproj.py
+++ mapproj.py
@@ -8,7 +8,7 @@ map = folium.Map(location = [38.85 , -99.09] , zoom_start=6 , tiles = "Stamen Te
 #map.add_child (folium.Marker (location = [38.2 , -99.1] , popup= "Hi I am a marker" , icon = folium.Icon(color = 'green') ))
 
 # Instead of above code you can use "FeatureGroup" as below: (for exampe "Marker" is a group)
-fg= folium.FeatureGroup (name = "My Map")
+fgv= folium.FeatureGroup (name = "Volcanoes")
 
 #reading markers from file
 data=pandas.read_csv("Volcanoes.txt" )
@@ -38,7 +38,6 @@ Height: %s m
 """
 
 
-
 # adding markers
 for lt , ln , el ,name in zip (lat,lon , elev ,name ): # zip () function distributes items 1by1
     #fg.add_child (folium.Marker (location = [lt , ln] , popup=str(el) + "m" , icon = folium.Icon(color = 'green') )) 
@@ -47,16 +46,29 @@ for lt , ln , el ,name in zip (lat,lon , elev ,name ): # zip () function distrib
     #iframe = folium.IFrame(html=html % str(el), width=200, height=100) #----Version#1 of popup-----
     iframe = folium.IFrame(html=html % (name , name, el), width=200, height=100) #----Version#2 of popup-----
 
-    #fg.add_child (folium.Marker (location = [lt , ln] , popup=folium.Popup(iframe) , icon = folium.Icon(color = "green") )) 
-    #fg.add_child (folium.Marker (location = [lt , ln] , popup=folium.Popup(iframe) , icon = folium.Icon(color = color_producer(el)  ) ))  # dynamic colors
+    #fg.add_child (folium.Marker (location = [lt , ln] , popup=folium.Popup(iframe) , icon = folium.Icon(color = "green") )) #----Version#3: adding color to marker-----
+    #fg.add_child (folium.Marker (location = [lt , ln] , popup=folium.Popup(iframe) , icon = folium.Icon(color = color_producer(el)  ) ))  # #----Version#4: adding dynamic colors to marker----- 
 
     # use Circle instead of marker
-    fg.add_child (folium.CircleMarker (location = [lt , ln] , popup=folium.Popup(iframe) , raduis = "1000" , icon = folium.Icon(color = color_producer(el)  ) )) 
+    fgv.add_child (folium.CircleMarker (location = [lt , ln] , popup=folium.Popup(iframe) , raduis = 6   , fill_color=color_producer(el) , color='grey'  , fill_opacity=0.7 )) 
 
     
 
+#adding a GeoJson Polygon layer
+fgp = folium.FeatureGroup (name = "Population")
+fgp.add_child (folium.GeoJson(data=  open('world.json' , 'r' , encoding = 'utf-8-sig').read()  ,
+style_function = lambda x: {'fillColor' : 'green'  if x['properties'] ['POP2005'] < 1000000  
+else 'orange'  if 10000000 <= x['properties'] ['POP2005'] <50000000 else 'red'} ) ) # Draws blue polygons around conutries
 
-map.add_child (fg)
+
+#finally
+map.add_child (fgv) 
+map.add_child (fgp)
+
+
+#layer control  (This way you could remove a layer if you want ). You need to add this line afte previous line: map.add_child (fg)
+#once you add layerControl you will see a button on right top corner of the map
+map.add_child(folium.LayerControl())
 
 map.save ("Map.html")
 
